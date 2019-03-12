67fc0fceecb730518b121c22b3aafc9f8ee19939
tkc49 <h.tkc49@gmail.com>
2019-03-12 13:36:22 +0900

    コメントの追加など

--- env/sample-dev_setting_deploy_app.js
+++ env/sample-dev_setting_deploy_app.js
@@ -1,37 +1,47 @@
 module.exports = {
-    "apps": {
-        "test": 247,
-        "test2": 248
+    /**********************************************************
+   *
+   * アプリの設定
+   * "任意の名前":アプリの番号
+   *
+   **********************************************************/
+    apps: {
+        test: 247,
+        test2: 248
     },
-    "props": {
-        "otherApiKey": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    props: {
+        otherApiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
     },
-    "contentsPath": "./app",
-    "contents": {
-        "test": {
-            "desktop": {
-                "js": [
+    /**********************************************************
+   *
+   * デプロイするファイルがあるディレクトリーを設定
+   *
+   **********************************************************/
+    contentsPath: "./app",
+    contents: {
+    // 上記の「アプリの設定」で設定した任意の名前ごとに kintone のデプロイのフォーマットに従い記述
+        test: {
+            desktop: {
+                js: [
                     "https://kintone.github.io/kintoneUtility/kintoneUtility.min.js",
                     "000-test/app.js",
                     "test.js"
                 ],
-                "css": []
+                css: []
             },
-            "mobile": {
-                "js": []
+            mobile: {
+                js: []
             }
         },
-        "test2": {
-            "desktop": {
-                "js": [
-                    "000-test/app.js"
-                ],
-                "css": []
+        // 上記の「アプリの設定」で設定した任意の名前ごとに kintone のデプロイのフォーマットに従い記述
+        test2: {
+            desktop: {
+                js: ["000-test/app.js"],
+                css: []
             },
-            "mobile": {
-                "js": []
+            mobile: {
+                js: []
             }
         }
-
     }
 };
