e2ff7dda224753c2d9238cebdbaf14fa4c28057b
tkc49 <h.tkc49@gmail.com>
2021-01-12 01:25:16 +0900

    Remove import of kintoneUtility

--- src/000-test/app.js
+++ src/000-test/app.js
@@ -1,21 +1,20 @@
-import kintoneUtility from "kintoneUtility";
-
 // テスト
 (function() {
+  kintone.events.on('app.record.index.show', function(event) {
+    console.log(event);
 
-    kintone.events.on('app.record.index.show', function(event) {
-        console.log(event);
-
-        kintoneUtility.rest.getAllRecordsByQuery({
-            app: 247
-            // query: '',
-            // fields: ['String', 'Number'],
-            // isGuest: false
-        }).then(function(response) {
-            console.log(response);
-        }).catch(function(error) {
-            console.log(error);
-        });
-    });
-
+    kintoneUtility.rest
+      .getAllRecordsByQuery({
+        app: 247,
+        // query: '',
+        // fields: ['String', 'Number'],
+        // isGuest: false
+      })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  });
 })();
